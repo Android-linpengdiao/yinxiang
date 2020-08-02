adc6248881dc441092ea33ca8ea9d7439de77c1a
linpengdiao <742094154@qq.com>
2020-08-02 23:31:46 +0800

    1

---
+++ app/src/main/java/com/yinxiang/view/MyWebView.java
@@ -0,0 +1,38 @@
+package com.yinxiang.view;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.webkit.WebView;
+
+public class MyWebView extends WebView {
+
+    public MyWebView(Context context) {
+        super(context);
+    }
+
+    public MyWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyWebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @SuppressLint("NewApi")
+    public MyWebView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public MyWebView(Context context, AttributeSet attrs, int defStyleAttr, boolean privateBrowsing) {
+        super(context, attrs, defStyleAttr, privateBrowsing);
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+        scrollTo(0, t);
+    }
+
+
+}
